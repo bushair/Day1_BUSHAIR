dcb162b50db9a3b33f18a9a2c02f6c505f2abb79
MacStudent <macstudent@126-iMac01.local>
2019-09-18 19:15:11 -0400

    Initial commit

---
+++ src/com/lambton/Day1.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+public class Day1
+{
+  public static void main(String[] args)
+  {
+      System.out.println("welcome To JAVA");
+      int a,b,c;
+      a=10;
+      b=20;
+      c =a+b;
+      System.out.println("SUM:"+c);
+
+  }
+}
